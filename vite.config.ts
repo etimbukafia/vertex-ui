4eb190aba3ca4aed4bf2d501b2864e10c2b634f5
Etimbuk Afia <ghostbandit02@gmail.com>
2025-05-22 09:24:53 +0100

    updated vite config to solve page not found error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/vertex-ui/", // ← REQUIRED for GitHub Pages project site
+  base: mode === 'production' ? '/vertex-ui/' : '/',
   server: {
     host: "::",
     port: 8080,
